f49300481fb4efb5695fe4e8e9d6550abffb362f
Charlotte Sarfati <charlotte@Charlottes-MacBook-Air.local>
2017-09-16 16:11:33 -0700

    falling snowflake feature complete

--- src/drawing/practice.clj
+++ src/drawing/practice.clj
@@ -3,22 +3,32 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+
 ;; setup 
 (defn setup []
+  (q/smooth)
   {:flake (q/load-image "images/white_flake.png")
-   :background (q/load-image "images/blue_background.png")})
+   :background (q/load-image "images/blue_background.png")
+   :y-param 10})
+  
+;; falling snowflake feature
+(defn update [state]
+  ;; updating y parameter by one for movement
+  (update-in state [:y-param] inc))
 
-;; used deconstructing to format this part; removes typing 'state'
-(defn draw [{flake :flake background :background}]
-  (q/background-image background)
-  (q/image flake 200 10))
+(defn draw [state]
+  ;; drawing blue background and a snowflake on it
+  (q/background-image (:background state))
+  (q/image (:flake state) 200 (:y-param state)))
+  
 
 (q/defsketch practice
   :title "Clara's Quil practice"
   :size [500 500]
   :setup setup
+  :update update
   :draw draw
   :features [:keep-on-top]
   :middleware [m/fun-mode])
-
+ 
 
